fb3f03842bdfb80af39c698a2530332911b89325
William Schneider <william.lima.schneider@gmail.com>
2023-09-20 17:33:56 -0300

    update readme

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
  pip install -r requirements.txt<br />
  python app.py
 </details>
+
+# Swagger
+ > Use [http://127.0.0.1:5000/ui/](http://127.0.0.1:5000/ui/) to access link
--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import connexion
 
 # Crie a aplicação connexion
-app = connexion.App(__name__, specification_dir='.')
+app = connexion.App(__name__, specification_dir=".")
 
 # Leia a especificação OpenAPI e crie os endpoints
-app.add_api('api_definition.yaml')
+app.add_api("api_definition.yaml")
 
-if __name__ == '__main__':
+
+@app.route("/")
+def hello():
+    return "Hellowfwefwe World!"
+
+
+if __name__ == "__main__":
     app.run(debug=True)
