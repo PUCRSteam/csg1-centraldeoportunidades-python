b5388aae80d77f1f8e206852718a6446b61615a9
William Schneider <william.lima.schneider@gmail.com>
2023-09-20 17:51:59 -0300

    added alchemy flask

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import connexion
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
 # Crie a aplicação connexion
 app = connexion.App(__name__, specification_dir=".")
 
 # Leia a especificação OpenAPI e crie os endpoints
 app.add_api("api_definition.yaml")
 
-
-@app.route("/")
-def hello():
-    return "Hellowfwefwe World!"
-
-
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# initialize the app with the extension
+db.init_app(app)
--- handlers.py
+++ handlers.py
@@ -3,30 +3,36 @@ def criar_vaga():
     print("Criando vaga")
     return "Vaga criada", 201
 
+
 def getVagaPorId(id):
     # Sua lógica aqui
     print("Vaga", id)
     return "Vaga", 200
+
 
 def getVagaPorAnunciante(idAnunciante):
     # Sua lógica aqui
     print("Vagas", idAnunciante)
     return "Vagas", 200
 
-def getVagasAlunoCurso(idCandidato,idCurso):
+
+def getVagasAlunoCurso(idCandidato, idCurso):
     # Sua lógica aqui
     print("Vagas", idCandidato, idCurso)
     return "Vagas", 200
+
 
 def editar_vaga(id):
     # Sua lógica aqui
     print("Editar vaga", id)
     return "Editar vaga", 200
 
+
 def criar_alerta(id):
     # Sua lógica aqui
     print("Criar alerta", id)
     return "Criar alerta", 200
+
 
 def getAlertasCandidato(idCandidato):
     # Sua lógica aqui
