5de0a31fef0aee1000f8635f2095f45d5b645b90
icaro espadim <10087971@pucrs.br>
2023-09-20 19:04:08 -0300

    app.app

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ app = connexion.App(__name__, specification_dir=".")
 app.add_api("api_definition.yaml")
 
 if __name__ == "__main__":
+    # configure the SQLite database, relative to the app instance folder
+    app.app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+    # initialize the app with the extension
+    db.init_app(app.app)
+    with app.app.app_context():
+        db.create_all()
     app.run(debug=True)
-
-
-# configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-# initialize the app with the extension
-db.init_app(app)
