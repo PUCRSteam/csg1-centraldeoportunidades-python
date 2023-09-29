af6b7ffba6ee51ab8d5134ea10f54d2198164720
khaelk <klkable13@gmail.com>
2023-09-29 02:25:43 -0300

    main app

--- app/main.py
+++ app/main.py
@@ -1,30 +1,21 @@
-from flask import Flask
-from dotenv import load_dotenv
-
-load_dotenv()
-
-from config import start_config
-from extensions import start_extensions
+import os
+from flask import Flask, request, jsonify
+from firebase_admin import credentials, firestore, initialize_app
+from app.extensions import start_extensions
 
 
-def create_app() -> Flask:
+def create_app():
     app = Flask(__name__)
-    
-    start_config(app)
-
     start_extensions(app)
-
-    with app.app_context():
-        from api import start_apis
-        start_apis(app, '/api')
-
-
+    @app.route("/")
+    def hello():        
+        return "hello"
     return app
 
 
+port = int(os.environ.get('PORT', 8080))
 
-if __name__ == "__main__":
-    app = create_app()    
-
-    app.run()
+if __name__ == '__main__':
+    app = create_app()
     
+    app.run(host='0.0.0.0', port=port, debug=True)
