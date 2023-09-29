0e7e725cfde8750d755af61ad8b45c9c9aa01691
khaelk <klkable13@gmail.com>
2023-09-28 23:31:03 -0300

    simplify errors and start doing firebase

--- app/errors/server/server_errors.py
+++
@@ -1,5 +0,0 @@
-from errors.exceptions import Error
-
-class ServerError(Error):
-    code = 500
-    msg = 'Unexpected error'
--- app/extensions/database.py
+++ app/extensions/database.py
@@ -1,3 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
+from firebase_admin import credentials, firestore, initialize_app
 
-db = SQLAlchemy()
+cred = credentials.Certificate('key.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+vagas_ref = db.collection('vagas')
+cursos_ref = db.collection('cursos')
