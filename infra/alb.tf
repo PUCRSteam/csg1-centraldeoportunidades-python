9ac8266b2eb80fba871ca65755f8ff970559012f
khaelk <66393713+khaelk@users.noreply.github.com>
2023-12-01 23:25:18 +0000

    adds alb

---
+++ infra/alb.tf
@@ -0,0 +1,69 @@
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "terraform-lb-csg1"
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.public_subnets
+
+  # Security Group
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+    all_https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      description = "HTTPS web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "10.0.0.0/16"
+    }
+  }
+
+  access_logs = {
+    bucket = "my-alb-logs"
+  }
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "h1"
+      protocol         = "HTTP"
+      port             = 8080
+      target_type      = "instance"
+    
+
+      protocol_version = "HTTP1"
+      target_id        = aws_instance.csg1-central-oportunidades.id
+      port             = 80
+      tags             = {
+        InstanceTargetGroupTag = "prod"
+      }
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "csg1"
+  }
+}
